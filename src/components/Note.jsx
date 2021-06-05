707ba67a8f34e26d71798881522bbf6ff28b4c30
Marcina <71164808+marcina2916@users.noreply.github.com>
2021-06-05 06:10:00 -0700

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & React JS</h1>
+      <p>
+        This was a BootCamp for JavaScript and React Js which is taught from the
+        Scratch including html. It was such an amazing bootcamp which was
+        conducted by Shaurya Sinha from ShapeAI.
+      </p>
+    </div>
+  );
+}
+export default Note;
